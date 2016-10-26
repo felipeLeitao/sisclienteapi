034a4fa0e2e522221be34f614060c68ceb6ac6f6
Felipe Leitão <fesilva@outlook.com.br>
2016-10-26 07:52:20 -0200

    Adicionei alguns campos na entidade de cliente

--- NetCoders.SisAmigos.RestFul/Controllers/ClienteController.cs
+++ NetCoders.SisAmigos.RestFul/Controllers/ClienteController.cs
@@ -1,5 +1,7 @@
 ﻿using NetCoders.SisAmigos.RestFul.Models;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace NetCoders.SisAmigos.RestFul.Controllers
@@ -13,9 +15,19 @@ namespace NetCoders.SisAmigos.RestFul.Controllers
         }
 
         [HttpPost]
-        public void Post(ClienteEntity cliente)
+        public HttpResponseMessage Post(ClienteEntity cliente)
         {
-            ClienteREP.Create(cliente);
+            if (ModelState.IsValid)
+            {
+                ClienteREP.Create(cliente);
+
+                return Request.CreateResponse(HttpStatusCode.OK, string.Format("{0} cadastrado com sucesso!", cliente.Nome));
+            }
+
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
         }
     }
 }
--- NetCoders.SisAmigos.RestFul/Models/ClienteEntity.cs
+++ NetCoders.SisAmigos.RestFul/Models/ClienteEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,22 @@ namespace NetCoders.SisAmigos.RestFul.Models
 {
     public class ClienteEntity
     {
+
         public Int32 Codigo { get; set; }
 
+        [Required(ErrorMessage = "O nome é obrigatório.")]
         public String Nome { get; set; }
 
         public String Email { get; set; }
+
+        public String CEP { get; set; }
+
+        public String Logradouro { get; set; }
+
+        public String Bairro { get; set; }
+
+        public String Cidade { get; set; }
+
+        public String UF { get; set; }
     }
 }
